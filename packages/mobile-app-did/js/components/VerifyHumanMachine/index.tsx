31209565895abfd628e75cb544fdfe56aafb4447
portkey-yellow <yellow.m@portkey.finance>
2023-05-04 11:47:34 +0800

    feat: 🎸 recaptcha time out

--- packages/mobile-app-did/js/components/VerifyHumanMachine/index.tsx
+++ packages/mobile-app-did/js/components/VerifyHumanMachine/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import RecaptchaModal from 'components/RecaptchaModal';
+import Recaptcha from 'components/Recaptcha';
 import OverlayModal from '../OverlayModal';
 import { screenWidth, screenHeight } from '@portkey-wallet/utils/mobile/device';
 import Config from 'react-native-config';
+import { clearBackgroundTimeout, setBackgroundTimeout } from 'utils/backgroundTimer';
+
+const TIME_OUT = 30000; // recaptcha timeout 30 seconds
 
 async function verifyHumanMachine(language: any) {
+  let timer: undefined | NodeJS.Timer;
   return new Promise((resolve, reject) => {
     OverlayModal.show(
-      <RecaptchaModal
+      <Recaptcha
         lang={language}
         headerComponent={null}
         siteKey={Config.RECAPTCHA_SITE_KEY}
@@ -22,6 +26,17 @@ async function verifyHumanMachine(language: any) {
         onClose={type => {
           OverlayModal.hide();
           if (type !== 'verified') reject();
+        }}
+        webViewProps={{
+          onLoadEnd: () => {
+            timer && clearBackgroundTimeout(timer);
+          },
+          onLoadStart: () => {
+            timer = setBackgroundTimeout(() => {
+              OverlayModal.hide();
+              reject('time out');
+            }, TIME_OUT);
+          },
         }}
         onError={error => {
           reject(error);
